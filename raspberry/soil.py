8cb7fc9faee6880eca40765b72100ac8efcaf95a
Unknown <asispinedo@opendeusto.es>
2020-12-29 12:47:49 +0100

    ScriptsRaspberry

--- raspberry/soil.py
+++ raspberry/soil.py
@@ -1,14 +1,28 @@
 import RPi.GPIO as GPIO
 import time
+from datetime import datetime
+import paho.mqtt.publish as publish
+import json
+
+
 channel=21
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
+regado=False
 def callback(channel):
     if GPIO.input(channel):
-        print("No se detecata agua")
+       regado=False
     else:
-        print("Si se detecta agua")
+        regado=True
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
 GPIO.add_event_callback(channel,callback)
 while True:
+    dateTimeObj = datetime.now()
+    topic = "iGarden/values" 
+    hostname = "test.mosquitto.org"
+    mensaje ={"Humedadtierra":{"value":regado,
+                     "TimeStamp":dateTimeObj}          
+              }
+    mensaje_json= json.dumps(mensaje)
+    publish.single(topic, mensaje_json, hostname=hostname)
     time.sleep(1)
